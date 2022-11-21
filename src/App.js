5fbf56991bdad517016f96f6ebe13b62c5278629
NatyCG <naty.l.araujo2@gmail.com>
2022-11-20 21:31:53 -0300

    Projeto Biscoito da Sorte

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      textoFrase: '',
+      imagem:'biscoito-removebg.png',
+      textoBotao: "Quebrar Biscoito",
+    };
+
+    this.quebrado = false;
+    this.quebrarBiscoito =  this.quebrarBiscoito.bind(this);
+
+    this.frases =[
+      'Siga os bons e aprenda com eles',
+      'O bom-senso vale mais do que muito conhecimento',
+      'O riso é a menor distância entre duas pessoas',
+      'Deixe de lado as preocupaçôes e seja feliz',
+      'Realize o óbvio, pense no improvável e conquiste o impossível',
+      'Acredite em milagres, mas não dependa deles',
+      'A maior barreira para o sucesso é o medo do fracasso',
+    ];
+  }
+
+  quebrarBiscoito(){
+    console.log(this.quebrado);
+    let state = this.state;
+    if(this.quebrado){
+      console.log("novo");
+      state.textoBotao = "Quebrar Biscoito";
+      state.textoFrase= "";
+      state.imagem = 'biscoito-removebg.png';
+    } else{
+      console.log("quebrado");
+    let state = this.state;
+    let numeroAleatorio = Math.floor(
+      Math.random() * this.frases.length
+      );
+    state.textoFrase = '"' + this.frases[numeroAleatorio] + '"';
+    state.imagem = "biscoito quebrado.png";
+    state.textoBotao = "Pedir outro biscoito";
+    }
+    this.setState(state);
+    this.quebrado = !this.quebrado;
+  }
+  render(){
+    return(
+       <div className='container'>
+        <img src={require(`./assets/${this.state.imagem}`)} alt="biscoito" className='imagem'></img>
+        <Botao nome ={this.state.textoBotao} acaoBtn={this.quebrarBiscoito}/>
+        <h3 className='textoFrase'>{this.state.textoFrase}</h3>
+      </div>
+    );
+  }
+ }
+
+ class Botao extends Component{
+  render(){
+    return(
+      <div>
+        <button className='botao btn' onClick={this.props.acaoBtn}>
+          {this.props.nome}
+        </button>
+      </div>
+    )
+  }
+ }
 
 export default App;
